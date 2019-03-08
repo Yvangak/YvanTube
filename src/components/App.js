b01281086ec44985ec175e6425ac4b6fb3a44083
Yvangak <gakyvan@gmail.com>
2019-03-08 15:08:17 -0600

    created a youtube api using axios. made a test call and inspected the results

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import '../styles/style.css';
+import React from 'react';
 
 class App extends React.Component {
 
+    onKeywordSubmit = (keyword) => {
+        youtube.get("/search", {
+            params: {
+                q: keyword
+            }
+        });
+    }
+
     render() {
         return (
-            <div className="ui container">
-                <SearchBar/>
+            <div className="ui container app">
+                <SearchBar search={this.onKeywordSubmit}/>
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,10 +9,15 @@ class SearchBar extends React.Component {
         this.setState({keyword: event.target.value});
     };
 
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.search(this.state.keyword);
+    }
+
     render(){
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="ui fluid icon input">
                         <input value={this.state.keyword} onChange={this.onInputChange} className="field" type="text" placeholder="Search Video..." />
                         <i className="search icon"></i>
