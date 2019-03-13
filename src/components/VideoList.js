a2f4bf4aecc9c8155572d725001d6fde03b4ea80
yvangak <gakyvan@gmail.com>
2019-03-13 11:21:19 -0500

    -The application now has a default keyword and videos  corresponding to it are fetched. -Whenever a user searches a term, the VideoList component  is updated and the VideoPlayer is given the first video  in the list of retrieved videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,8 @@ import VideoItem from './VideoItem';
 import React from 'react';
 
 const VideoList = ({videos, select}) => {
-    const videoItems = videos.map((video, index) => {
-        return <VideoItem select={select} key={index} video={video}/>;
+    const videoItems = videos.map((video) => {
+        return <VideoItem select={select} key={video.id.videoId} video={video}/>;
     });
     return (
         <div className="ui segment">{videoItems}</div>
